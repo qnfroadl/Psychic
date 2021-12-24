ff4f1506532c6ebec7ad267718ebb52565d9bab8
qnfroadl <qnfroadl@gmail.com>
2021-12-24 15:03:08 +0900

    빈프로젝트 생성

---
+++ Source/Psychic/PsychicGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PsychicGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PSYCHIC_API APsychicGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
